29a67b814b3a6a9daecb8b213420f6dcf0669a10
doga-ozsoyler <doga.ozsoyler@gmail.com>
2024-11-12 21:53:15 +0300

    feat(StepOne): Add input

--- src/pages/StepOne/index.tsx
+++ src/pages/StepOne/index.tsx
@@ -3,10 +3,12 @@ import "./StepOne.css";
 const StepOne = () => {
   return (
     <div className="container">
-      <div className="form">
+      <div className="form-container">
         <p className="text">
           Let’s hear more about you to prepare your personal workout plan
         </p>
+        <input type="number" placeholder="Your height" />
+        <input type="number" placeholder="Your weight" />
       </div>
     </div>
   );
