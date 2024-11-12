e5883b66ccbd1a44203b41a253eb3eddba104fdb
doga-ozsoyler <doga.ozsoyler@gmail.com>
2024-11-12 21:26:09 +0300

    feat(StepOne): Add text

---
+++ src/pages/StepOne/index.tsx
@@ -0,0 +1,15 @@
+import "./StepOne.css";
+
+const StepOne = () => {
+  return (
+    <div className="container">
+      <div className="form">
+        <p className="text">
+          Let’s hear more about you to prepare your personal workout plan
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StepOne;
