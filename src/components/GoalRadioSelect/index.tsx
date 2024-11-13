7be35988d4c3dc0a1da911ecf1aed161f6c18be5
doga-ozsoyler <doga.ozsoyler@gmail.com>
2024-11-13 14:04:18 +0300

    feat(GoalRadioSelect): Add icon in label

--- src/components/GoalRadioSelect/index.tsx
+++ src/components/GoalRadioSelect/index.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import "./GoalRadioSelect.css";
+import FlameIcon from "../icons/FlameIcon";
+import LevelIcon from "../icons/LevelIcon";
+import SmileIcon from "../icons/SmileIcon";
 
 interface GoalRadioSelectProps {
   disabledDays?: string[];
 }
 
-const goals = ["Lose weight", "Build muscle", "Stay healthy"];
+const goals = [
+  { icon: <FlameIcon />, name: "Lose weight" },
+  { icon: <LevelIcon />, name: "Build muscle" },
+  { icon: <SmileIcon />, name: "Stay healthy" },
+];
 
 const GoalRadioSelect: React.FC<GoalRadioSelectProps> = () => {
   const [selected, setSelected] = useState<string>("");
@@ -14,16 +21,23 @@ const GoalRadioSelect: React.FC<GoalRadioSelectProps> = () => {
     <div className="ratio-container">
       {goals.map((goal, index) => (
         <div
-          key={goal}
+          key={goal.name}
           className="ratio-item"
-          onClick={() => setSelected(goal)}
+          onClick={() => setSelected(goal.name)}
           style={{
             borderBottom:
               index !== goals.length - 1 ? "1px solid #9B9BEB" : "none",
           }}
         >
-          <label>{goal}</label>
-          <input type="radio" checked={selected === goal} onChange={() => {}} />
+          <label className="ratio-label">
+            {goal.icon}
+            {goal.name}
+          </label>
+          <input
+            type="radio"
+            checked={selected === goal.name}
+            onChange={() => {}}
+          />
         </div>
       ))}
     </div>
